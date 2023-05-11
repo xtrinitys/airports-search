4865c7c6f6672e19a4909c777c7e45246d559883
Maxim <slimbox.sdb@gmail.com>
2023-05-11 09:29:40 +0600

    ref(): moved getQuery method out of App

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Console application (JDK 11) that allows you to quickly search
 airport data by user-entered airport name and filters.
 
+**Warning:** `null` in program output equals to `\N` in CSV file
+
 # Requirements
 - `Java 11`
 - `Maven`
--- src/main/java/com/xtrinity/App.java
+++ src/main/java/com/xtrinity/App.java
@@ -1,13 +1,9 @@
 package com.xtrinity;
 
 import com.xtrinity.dto.UserInputDto;
-import com.xtrinity.entities.search.SearchQuery;
 import com.xtrinity.entities.search.SearchResult;
 import com.xtrinity.exceptions.AppException;
-import com.xtrinity.exceptions.WrongFilterSyntaxException;
-import com.xtrinity.exceptions.WrongInputException;
 import com.xtrinity.services.IOService;
-import com.xtrinity.services.search.SearchParserService;
 import com.xtrinity.services.search.SearchService;
 
 public class App {
@@ -24,22 +20,26 @@ public class App {
             try {
                 UserInputDto userInput = ioService.getUserInput();
 
+//                UserInput = "!quit"
                 if (userInput == null) {
                     return;
                 }
 
-                SearchQuery query = SearchParserService.parseQuery(userInput);
+                SearchResult result = searchService.searchFor(userInput);
 
-                SearchResult result = searchService.searchFor(query);
+                printResults(result);
 
-                int totalRows = searchService.getLastSearchTotalRows();
-                long searchDuration = searchService.getLastSearchTime();
-
-                ioService.printResults(result, searchDuration, totalRows);
             } catch (AppException ex) {
                 handleException(ex);
             }
         }
+    }
+
+    private void printResults(SearchResult result) {
+        int totalRows = searchService.getLastSearchTotalRows();
+        long searchDuration = searchService.getLastSearchTime();
+
+        ioService.printSearchResults(result, searchDuration, totalRows);
     }
 
     private void handleException(Exception ex) {
